d4db9550aca9f49971322f7bd7a3169161d4c34e
Beom Jin Lee <cluesbj@berkeley.edu>
2020-09-28 16:11:01 +0900

    Refactor and complete KSEA Token

--- KSEAToken/KSEAToken.sol
+++ KSEAToken/KSEAToken.sol
@@ -19,6 +19,7 @@ contract KSEAToken is EIP20Interface {
     /// Public variables of the overall balance and allowance 
     mapping (address => uint256) public balances;
     mapping (address => mapping (address => uint256)) public allowed;
+    string public name;
     
     /// @notice Constructor to initialize KSEA Tokens by setting name and balance 
     /// @param _initialSupply The initial supply of tokens
@@ -28,8 +29,14 @@ contract KSEAToken is EIP20Interface {
         name = "KSEA Token";                                 // Set the name of token to 'KSEA Token'
     }
 
+    /// @notice Returns the name of the Token 
+    function name() public view returns (string) { 
+        return name; 
+    }
+
     function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balances[msg.sender] >= _value); 
+        require(balances[msg.sender] >= _value && value < 10); // Ensure number of tokens sent is less than 10 
+        require(_to != address(0));
         balances[msg.sender] -= _value;
         balances[_to] += _value;
         emit Transfer(msg.sender, _to, _value); 
@@ -37,18 +44,27 @@ contract KSEAToken is EIP20Interface {
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value);
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        allowed[_from][msg.sender] -= _value; // Decrement allowance 
+        emit Transfer(_from, _to, _value); 
+        return true;
     }
 
     function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner]; 
     }
 
     function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(value < 10); 
+        require(_spender != address(0));
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value); 
+        return true;
     }
 
     function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
-    }
-
-
-    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return allowed[_owner][_spender];
     }
 }
