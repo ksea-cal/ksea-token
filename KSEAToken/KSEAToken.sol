2e965c3b21b19f2d06d706288dd3823ee96e3b10
Beom Jin Lee <cluesbj@berkeley.edu>
2020-10-05 00:28:54 +0900

    Downgrade to 0.4.12 and add SafeMath

--- KSEAToken/KSEAToken.sol
+++ KSEAToken/KSEAToken.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 
 /** 
 * @title KSEA Standard EIP 20 Token 
@@ -9,10 +9,10 @@
 */
 
 
-pragma solidity ^0.7.0;
+pragma solidity ^0.4.12;
 
 import "./EIP20Interface.sol";
-import "github.com/OpenZeppelin/zeppelin-solidity/contracts/math/SafeMath.sol";
+import "./SafeMath.sol";
 
 /// @notice KSEA Token Contract by implementing EIP20 Interface
 contract KSEAToken is EIP20Interface {
@@ -23,21 +23,15 @@ contract KSEAToken is EIP20Interface {
     mapping (address => mapping (address => uint256)) public allowed;
     string public name;
     
-    /// @notice Constructor to initialize KSEA Tokens by setting name and balance 
-    /// @param _initialSupply The initial supply of tokens
-    function KSEAToken(uint256 _initialSupply) public {
+    constructor(uint256 _initialSupply) public {
         balances[msg.sender] = _initialSupply;               // Give the creator all initial tokens
         totalSupply = _initialSupply;                        // Update total supply
         name = "KSEA Token";                                 // Set the name of token to 'KSEA Token'
     }
 
-    /// @notice Returns the name of the Token 
-    function name() public view returns (string) { 
-        return name; 
-    }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balances[msg.sender] >= _value && value < 10); // Ensure number of tokens sent is less than 10 
+        require(balances[msg.sender] >= _value && _value < 10); // Ensure number of tokens sent is less than 10 
         require(_to != address(0));
         balances[msg.sender].sub(_value);
         balances[_to].add(_value);
@@ -59,7 +53,7 @@ contract KSEAToken is EIP20Interface {
     }
 
     function approve(address _spender, uint256 _value) public returns (bool success) {
-        require(value < 10); 
+        require(_value < 10); 
         require(_spender != address(0));
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value); 
