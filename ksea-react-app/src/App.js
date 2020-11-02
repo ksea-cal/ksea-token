01fdfac8ee4fcbc99e9ee2bf66c3d63b5adcaa27
Simon <simony1124@gmail.com>
2020-11-02 16:59:33 +0900

    frontend skeleton

--- ksea-react-app/src/App.js
+++ ksea-react-app/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Router path="/checkout">
+            <h1> Check out</h1>
+          </Router>
+          <Route path="/login">
+            <h1> Log-in page</h1>
+          </Route>
+          <Route path="/">
+            <h1> Home Page!!!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+{/*https://www.youtube.com/watch?v=1_IYL9ZMR_Y&t=18s*/}
