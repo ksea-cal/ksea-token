112e0b0e058c6619c708bea62ee4a85bc43467e2
Beom Jin Lee <cluesbj@berkeley.edu>
2020-11-23 07:47:58 +0900

    add modified backend

--- ksea-token/ksea-token/ranking.py
+++ ksea-token/ksea-token/ranking.py
@@ -2,12 +2,10 @@ from flask import *
 from web3 import Web3
 import ast
 import time
+import datetime
 
 from app import app
 from db import db, User, Checkin
-
-
-CURR_CID = 0 
 
 
 @app.route("/")
@@ -37,22 +35,60 @@ def award():
             awardee = User.query.filter(User.address == hex(address)).first() 
             if not awardee: 
                 raise Exception("[ERROR] No Awardee {}".format(address))
-            else: 
-                print(awardee)
             awardee.num_points += points
         db.session.commit()
 
 
-@app.route("/createcheckin", methods=["POST"])
-def create_check_in():
+@app.route("/awardevent", methods=["POST"])
+def award_event():
     """ Award points to members """ 
+    if request.method == "POST":
+        event = request.values['event']
+        event_to_award = Event.query.filter(Event.event_name == event)
+        members = Checkin.query.filter(Checkin.event == event_to_award.eid).all() 
+        awardees = [m.user for m in members]
+        awardee_names = [User.query.filter(User.uid == an).first().name for an in awardees]
+        for u in awardees: 
+            awardee = User.query.filter(User.uid == u).first() 
+            if not awardee: 
+                raise Exception("[ERROR] No Awardee {}".format(address))
+            awardee.num_points += points
+        db.session.commit()
+        return {"awardees": awardee_names}
+
+
+@app.route("/createcheckin", methods=["POST"])
+def create_checkin():
+    """ Create a Checkin Event with Password """ 
     if request.method == "POST":
         password = request.values['password']
         time_limit = int(request.values['timeLimit'])
         event_name = request.values['eventName']
-        curr_checkin = Checkin(event_name=event_name,  password=password, time_limit=time_limit)
+        curr_checkin = Event(event_name=event_name,  password=password, time_limit=time_limit)
         db.session.add(curr_checkin)
         db.session.commit()
+
+
+@app.route("/checkin", methods=["GET", "POST"])
+def checkin(): 
+    curr_status = "waiting"
+    if request.method == "POST":
+        curr_checkin = Event.query.order_by(Checkin.cid.desc()).first()
+        if curr_checkin.time_limit + curr_checkin.current_time > datetime.datetime.utcnow:  
+            input_password = request.values['password']
+            if input_password == curr_checkin.password:  
+                address = int(request.values['address'])
+                awardee = User.query.filter(User.address == hex(address)).first() 
+                curr_checkin = Checkin(event=curr_checkin.eid, user=awardee.uid)
+                db.session.add(curr_checkin)
+                db.session.commit()
+                curr_status = "success"
+            else: 
+                curr_status = "wrong_password"
+        else: 
+            curr_status = "overtime"
+    else: 
+        return {"curr_status": curr_status}
 
 
 @app.route("/auction", methods=["POST", "GET"])
