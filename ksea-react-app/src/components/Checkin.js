b962ec43375dea0c80047be5a69c08013dc47779
Beom Jin Lee <31300786+beomjin-lee@users.noreply.github.com>
2021-01-03 06:59:39 -0800

    Add backend compatibility with App.js (#17)
    * committing for merge conflict
    
    * add modified backend
    
    * remove auction builds
    
    * Delete Airdrop build
    
    * add member and winner components
    
    * Rewrite README
    
    * Rewrite README
    
    * add checkin
    
    * resolve merge conflict
    
    * resolve merge conflict

--- ksea-react-app/src/components/Checkin.js
+++ ksea-react-app/src/components/Checkin.js
@@ -2,42 +2,51 @@ import React from "react";
 import { Button, Form } from "react-bootstrap";
 
 function Checkin(props) {
+
+  function handlePassword(event) { 
+    setPassword(event.target.value);
+  }
+
+  function handleAddress(event) { 
+    setAddress(event.target.value);
+  }
+
+  function handleSubmitCheckin(event) { 
+    let formData = new FormData();
+    formData.append('password', password); 
+    formData.append('address', address); 
+    axios.post("http://127.0.0.1:5000/checkin", formData)
+        .then(res => res.json()).then(
+          data => { 
+            setStatus(data.curr_status)
+        })
+    if (status == 'success') { 
+      window.alert("Successfully Checked In")
+    } else if (status == 'wrong_password') { 
+      window.alert("Wrong Password")
+    } else if (status == 'overtime') { 
+      window.alert("Over time")
+    } else { 
+      window.alert("Failed for unknown reasons")
+    }
+  }
+
+  const [password, setPassword] = useState('');
+  const [address, setAddress] = useState('');
+  const [status, setStatus] = useState('');
+
   return (
     <Form>
-      <Form.Group controlId="formName">
-        <Form.Label>Name</Form.Label>
-        <Form.Control type="email" placeholder="Bumjin Jjang" />
-        <Form.Text className="text-muted"></Form.Text>
-      </Form.Group>
-
-      <Form.Group controlId="formKeyword">
-        <Form.Label>Keyword</Form.Label>
-        <Form.Control type="password" placeholder="keyword placeholder" />
-      </Form.Group>
       <div className="form-group">
-        <label for="exampleFormControlSelect1">Rate</label>
-        <select className="form-control" id="exampleFormControlSelect1">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </select>
+          <Form.Group>
+            <Form.Label>Create Check-In</Form.Label>
+            <Form.Control type="text" onChange={handleAddress} placeholder="Address (e.g., 0x...)" /> 
+            <Form.Control type="text" onChange={handlePassword} placeholder="Password" /> 
+          </Form.Group>
+          <Button onClick={handleSubmitCheckin}  variant="primary" type="submit">
+            Create Check-In
+          </Button>
       </div>
-
-      <div class="form-group">
-        <label for="exampleFormControlTextarea1">Feedback</label>
-        <textarea
-          class="form-control"
-          id="exampleFormControlTextarea1"
-          rows="3"
-          placeholder="It suckedddd"
-        ></textarea>
-      </div>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
     </Form>
   );
 }
